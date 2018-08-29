af6ad08443e847ea3519d71f02cd13ec1ea4b0fe
Henry Hobhouse <henry.hobhouse@divido.com>
2018-08-29 17:29:53 +0100

    Added Docs to Game Component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -25,7 +25,6 @@ type State = {
     stepNumber: number,
 }
 
-
 /**
  * Game React Module. In charge of state and game logic.
  */
@@ -41,7 +40,7 @@ export class Game extends React.Component<Props, State> {
     };
 
     /**
-     * initialises array of #param:numberOfSquares game squares with value of null and returns.
+     * Initialises array of squares as a Board
      * @param {number} numberOfSquares
      * @returns {Board}
      */
@@ -49,7 +48,12 @@ export class Game extends React.Component<Props, State> {
         return Array(numberOfSquares).fill(null)
     }
 
-    static calculateWinner(squares: Array<SquareContent>): SquareContent {
+    /**
+     * Checks if Board has any winning lines
+     * @param {Board} squares
+     * @returns {SquareContent} null | "X" | "Y"
+     */
+    static calculateWinner(squares: Board): SquareContent {
         for (const line of winningLines) {
             const [a, b, c] = line;
             if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -59,10 +63,19 @@ export class Game extends React.Component<Props, State> {
         return null;
     }
 
+    /**
+     * Gets current player from conditional field
+     * @param {boolean} condition
+     * @returns {PlayerName} "X" | "Y"
+     */
     static getPlayer(condition: boolean): PlayerName {
         return condition ? player1 : player2;
     }
 
+    /**
+     * Renders the GameStatus React module
+     * @returns {ReactElement<typeof GameStatus>}
+     */
     renderStatus(): ReactElement<typeof GameStatus> {
         const { winner, currentPlayer } = this.state;
         let status;
@@ -74,19 +87,27 @@ export class Game extends React.Component<Props, State> {
         return <GameStatus>{status}</GameStatus>
     }
 
+    /**
+     * Renders GameBoard React Component
+     * @returns {ReactElement<typeof GameBoard>}
+     */
     renderBoard(): ReactElement<typeof GameBoard> {
         const { currentPlayer, winner, history, stepNumber } = this.state;
         const currentSquares = history[stepNumber].move;
         return (
             <GameBoard squares={ currentSquares }
                        currentPlayer={ currentPlayer }
-                       onChange={ (squares: Board) => this.onChange(squares) }
+                       onChange={ (squares: Board) => this.onNewMove(squares) }
                        winner={ winner }
             />
         )
     }
 
-    onChange(squares: Board): void {
+    /**
+     * Updates state of History, stepNumber, currentPlayer and winner
+     * @param {Board} squares
+     */
+    onNewMove(squares: Board): void {
         const {currentPlayer, history, stepNumber} = this.state;
         const updatedHistory = history.slice(0, stepNumber + 1);
         const nextPlayer = Game.getPlayer(currentPlayer !== player1);
@@ -100,15 +121,37 @@ export class Game extends React.Component<Props, State> {
         });
     }
 
+    /**
+     * Renders GameHistory React Component
+     * @returns {ReactElement<typeof GameHistory>}
+     */
     renderHistory(): ReactElement<typeof GameHistory> {
         return (
             <GameHistory
                 history={this.state.history}
-                jumpTo={ (move) => this.jumpTo(move) }
+                jumpTo={ (move) => this.jumpToMove(move) }
             />
         )
     }
 
+    /**
+     * Updates state of stepNumber, currentPlayer and winner value based on step parameter
+     * @param {number} step
+     */
+    jumpToMove(step: number): void {
+        const {history} = this.state;
+        const winnerUpdate = Game.calculateWinner(history[step].move);
+        this.setState({
+            stepNumber: step,
+            currentPlayer: Game.getPlayer((step % 2) === 0),
+            winner: winnerUpdate,
+        })
+    }
+
+    /**
+     * Renders this components React Element
+     * @returns {ReactElement<HTMLElement>}
+     */
     render(): ReactElement<HTMLElement> {
         return (
             <div className="game">
@@ -121,16 +164,6 @@ export class Game extends React.Component<Props, State> {
                 </div>
             </div>
         );
-    }
-
-    jumpTo(step: number): void {
-        const {history} = this.state;
-        const winnerUpdate = Game.calculateWinner(history[step].move);
-        this.setState({
-            stepNumber: step,
-            currentPlayer: Game.getPlayer((step % 2) === 0),
-            winner: winnerUpdate,
-        })
     }
 
 }
